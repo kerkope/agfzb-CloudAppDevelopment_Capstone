dd68e14fd80c57b6db47306e54c37abc7f96bcf4
kerkope <hunterschwager@gmail.com>
2022-08-21 17:14:04 +0000

    Changes 8/21

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -29,7 +29,7 @@ APPEND_SLASH = True
 
 ALLOWED_HOSTS = [
     "localhost",
-    "hunterschwagercardealer.us-east.cf.appdomain.cloud",
+    "hunterschwagercardealer.mybluemix.net",
     "127.0.0.1",
 ]
 
