8d2fa54475c10876a8afd0dfac5e62abea384775
kerkope <hunterschwager@gmail.com>
2022-08-23 22:48:28 +0000

    Deployed, Working on Reviews

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -13,22 +13,16 @@ urlpatterns = [
     # view refers to the view function
     # name the URL
 
-    # path for about view
-
-    # path for contact us view
-
-    # path for registration
-
-    # path for login
-
-    # path for logout
-
     path('',views.HomePageView.as_view(),name='home'),
     path('about/',views.AboutPageView.as_view(),name='about'),
     path('contact/',views.ContactPageView.as_view(),name='contact'),
+    path('reviews/', views.ReviewPageView.as_view(),name='reviews'),
     path('signup/',views.SignUpView.as_view(),name='signup'),
     path('logout/', views.logout_request, name='logout'),
     path('login/', views.login_request, name='login'),
+    path(route='dealer/<int:id>/', view=views.get_dealer_details, name='dealer_details'),
+    path(route='dealer/<int:id>/review', view=views.add_review, name='add_review'),
+
 
     # path for dealer reviews view
 
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -26,6 +26,9 @@ class HomePageView(TemplateView):
 class AboutPageView(TemplateView):
     template_name = 'about.html' 
 
+class ReviewPageView(TemplateView):
+    template_name = 'reviews.html'
+
 class ContactPageView(TemplateView):
     template_name = 'contact.html' 
 
@@ -86,10 +89,65 @@ def register(request):
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 
 def get_dealerships(request):
-    context = {}
     if request.method == "GET":
+        context = {}
+        url = "https://53c4f092.eu-gb.apigw.appdomain.cloud/api/dealerships"
+        dealerships = get_dealers_from_cf(url)
+        context["dealership_list"] = dealerships
         return render(request, 'djangoapp/index.html', context)
 
+def get_dealer_details(request, id):
+    if request.method == "GET":
+        context = {}
+        dealer_url = "https://53c4f092.eu-gb.apigw.appdomain.cloud/api/dealerships"
+        dealer = get_dealer_by_id_from_cf(dealer_url, id=id)
+        context["dealer"] = dealer
+    
+        review_url = "https://53c4f092.eu-gb.apigw.appdomain.cloud/api/review"
+        reviews = get_dealer_reviews_from_cf(review_url, id=id)
+        print(reviews)
+        context["reviews"] = reviews
+        
+        return render(request, 'djangoapp/dealer_details.html', context)
+
+def add_review(request, id):
+    context = {}
+    dealer_url = "https://53c4f092.eu-gb.apigw.appdomain.cloud/api/dealerships"
+    dealer = get_dealer_by_id_from_cf(dealer_url, id=id)
+    context["dealer"] = dealer
+    if request.method == 'GET':
+        # Get cars for the dealer
+        cars = CarModel.objects.filter(id=id)
+        print(cars)
+        context["cars"] = cars
+        
+        return render(request, 'djangoapp/add_review.html', context)
+    elif request.method == 'POST':
+        if request.user.is_authenticated:
+            username = request.user.username
+            print(request.POST)
+            payload = dict()
+            car_id = request.POST["car"]
+            car = CarModel.objects.get(pk=car_id)
+            payload["time"] = datetime.utcnow().isoformat()
+            payload["name"] = username
+            payload["dealership"] = id
+            payload["id"] = id
+            payload["review"] = request.POST["content"]
+            payload["purchase"] = False
+            if "purchasecheck" in request.POST:
+                if request.POST["purchasecheck"] == 'on':
+                    payload["purchase"] = True
+            payload["purchase_date"] = request.POST["purchasedate"]
+            payload["car_make"] = car.make.name
+            payload["car_model"] = car.name
+            payload["car_year"] = int(car.year.strftime("%Y"))
+
+            new_payload = {}
+            new_payload["review"] = payload
+            review_post_url = "https://53c4f092.eu-gb.apigw.appdomain.cloud/api/review"
+            post_request(review_post_url, new_payload, id=id)
+        return redirect("djangoapp:dealer_details", id=id)
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
 # def get_dealer_details(request, dealer_id):
